f8c4a5e5c4e0c8279a38a038552c8ddf13b1b29c
earny4302 <73746161+earny4302@users.noreply.github.com>
2024-05-06 12:57:34 +0530

    1st Commit
    Completed half side bar and total amount

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
-
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 const Home = () => {
+  const loggedIn = {firstName: 'Priyanshu'};
   return (
-    <div>
-      dashboad
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type = "greeting"
+            title = "Welcome"
+            user = {loggedIn?.firstName || 'Guest'}
+            subtext = "Access and manage your acount and transactions efficiently."
+          />
+        </header>
+        <TotalBalanceBox 
+          accounts = {[]}
+          totalBanks = {1}
+          totalCurrentBalance = {1250.35}
+        />
+
+      </div>
+    </section>
   )
 }
 
