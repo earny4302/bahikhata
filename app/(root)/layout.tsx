af342db286a39bfc65a5eb8a47094b1f9a1e1fe5
earny4302 <73746161+earny4302@users.noreply.github.com>
2024-05-06 17:45:56 +0530

    k

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import Sidebar from "@/components/Sidebar";
-
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -8,8 +9,14 @@ export default function RootLayout({
   const loggedIn = {firstName: 'Priyanshu', lastName: 'Chourasiya',}
   return (
     <main className="flex h-screen w-full font-inter">
-        <Sidebar user = {loggedIn} />
-        {children}
+        {/* <Sidebar user = {loggedIn} /> */}
+        {/* <div className="flex size-full flex-col">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user ={loggedIn} />
+          </div>
+       
+        </div> */}' {children}'
     </main>
   );
 }
