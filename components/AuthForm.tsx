00b72433f5beb426ebdb71b31138151a4d584e36
earny4302 <73746161+earny4302@users.noreply.github.com>
2024-05-21 01:13:24 +0530

    complete finalize plaid and dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,15 +22,14 @@ import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { signIn, signUp } from '@/lib/actions/user.actions';
-//import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
-//import PlaidLink from './PlaidLink';
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  
+
   const formSchema = authFormSchema(type);
 
     // 1. Define your form.
@@ -45,39 +44,47 @@ const AuthForm = ({ type }: { type: string }) => {
     // 2. Define a submit handler.
     const onSubmit = async (data: z.infer<typeof formSchema>) => {
       setIsLoading(true);
-      try{
-        if(type ==='sign-up')
-          {
-            const newUser = await signUp(data);
-            setUser(newUser);
-          }
+
+      try {
+        // Sign up with Appwrite & create plaid token
         
-      
-          if(type==='sign-in')
-            {
-              const response = await signIn({
-                email: data.email,
-                password: data.password,
-              })
-              if(response)
-                {
-                  router.push('/')
-                }
-            }
-          
-      }catch(error)
-      {
+        if(type === 'sign-up') {
+          const userData = {
+            firstName: data.firstName!,
+            lastName: data.lastName!,
+            address1: data.address1!,
+            city: data.city!,
+            state: data.state!,
+            postalCode: data.postalCode!,
+            dateOfBirth: data.dateOfBirth!,
+            ssn: data.ssn!,
+            email: data.email,
+            password: data.password
+          }
+
+          const newUser = await signUp(userData);
+
+          setUser(newUser);
+        }
+
+        if(type === 'sign-in') {
+          const response = await signIn({
+            email: data.email,
+            password: data.password,
+          })
+
+          if(response) router.push('/')
+        }
+      } catch (error) {
         console.log(error);
       } finally {
         setIsLoading(false);
       }
-
-         }
+    }
 
   return (
-    <div className="flex">
-    <section className="auth-form flex  ">
-      <header className='flex flex-col gap-5 items-center  md:gap-8'>
+    <section className="auth-form">
+      <header className='flex flex-col gap-5 md:gap-8'>
           <Link href="/" className="cursor-pointer flex items-center gap-1">
             <Image 
               src="/icons/logo.svg"
@@ -85,11 +92,11 @@ const AuthForm = ({ type }: { type: string }) => {
               height={34}
               alt="Horizon logo"
             />
-            <h1 className="text-36 font-ibm-plex-serif font-bold text-black-1">BahiKhata</h1>
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
           </Link>
 
           <div className="flex flex-col gap-1 md:gap-3">
-            <h1 className="text-16 lg:text-26 flex flex-col items-center font-bold text-gray-900">
+            <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
               {user 
                 ? 'Link Account'
                 : type === 'sign-in'
@@ -107,25 +114,25 @@ const AuthForm = ({ type }: { type: string }) => {
       </header>
       {user ? (
         <div className="flex flex-col gap-4">
-          {/* <PlaidLink user={user} variant="primary" /> */}
+          <PlaidLink user={user} variant="primary" />
         </div>
       ): (
         <>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
               {type === 'sign-up' && (
                 <>
                   <div className="flex gap-4">
                     <CustomInput control={form.control} name='firstName' label="First Name" placeholder='Enter your first name' />
-                    <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your last name' />
+                    <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your first name' />
                   </div>
                   <CustomInput control={form.control} name='address1' label="Address" placeholder='Enter your specific address' />
                   <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city' />
-                  <div className="flex gap-8">
+                  <div className="flex gap-4">
                     <CustomInput control={form.control} name='state' label="State" placeholder='Example: NY' />
                     <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: 11101' />
                   </div>
-                  <div className="flex gap-8">
+                  <div className="flex gap-4">
                     <CustomInput control={form.control} name='dateOfBirth' label="Date of Birth" placeholder='YYYY-MM-DD' />
                     <CustomInput control={form.control} name='ssn' label="SSN" placeholder='Example: 1234' />
                   </div>
@@ -136,7 +143,7 @@ const AuthForm = ({ type }: { type: string }) => {
 
               <CustomInput control={form.control} name='password' label="Password" placeholder='Enter your password' />
 
-              <div className="flex flex-col gap-1">
+              <div className="flex flex-col gap-4">
                 <Button type="submit" disabled={isLoading} className="form-btn">
                   {isLoading ? (
                     <>
@@ -150,7 +157,7 @@ const AuthForm = ({ type }: { type: string }) => {
             </form>
           </Form>
 
-          <footer className="flex justify-center gap-2">
+          <footer className="flex justify-center gap-1">
             <p className="text-14 font-normal text-gray-600">
               {type === 'sign-in'
               ? "Don't have an account?"
@@ -163,7 +170,6 @@ const AuthForm = ({ type }: { type: string }) => {
         </>
       )}
     </section>
-    </div>
   )
 }
 
