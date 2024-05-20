28d050b8711a1fdb982ea5f1db93910127d39088
earny4302 <73746161+earny4302@users.noreply.github.com>
2024-05-20 22:27:24 +0530

    Revert "k"
    This reverts commit ed3b56a3db9db99096758a1d2474a6a304cca161.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,24 +2,9 @@ import React from 'react'
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import RightSidebar from '@/components/RightSidebar';
-
-
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 const Home= async() => {
-
-
   const loggedIn = await getLoggedInUser();
-
-
-
-
-
-
-
-
-
-
-
   return (
     <section className="home">
       <div className="home-content">
@@ -34,23 +19,16 @@ const Home= async() => {
           accounts = {[]}
           totalBanks = {1}
           totalCurrentBalance = {1250.35}
-
           />
-
 
           </header>
           RECent TRANSACTIONS
-
-
-
-
       </div>
       <RightSidebar user={loggedIn}
       transaction ={[]}
       banks={[{currentBalance: 124.50},{currentBalance: 500.23}]} />
-
-
-
     </section>
   )
 }
+
+export default Home
