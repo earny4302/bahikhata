ed3b56a3db9db99096758a1d2474a6a304cca161
earny4302 <73746161+earny4302@users.noreply.github.com>
2024-05-20 22:27:06 +0530

    k

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import RightSidebar from '@/components/RightSidebar';
+
+
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 const Home= async() => {
+
+
   const loggedIn = await getLoggedInUser();
+
+
+
+
+
+
+
+
+
+
+
   return (
     <section className="home">
       <div className="home-content">
@@ -19,16 +34,23 @@ const Home= async() => {
           accounts = {[]}
           totalBanks = {1}
           totalCurrentBalance = {1250.35}
+
           />
+
 
           </header>
           RECent TRANSACTIONS
+
+
+
+
       </div>
       <RightSidebar user={loggedIn}
       transaction ={[]}
       banks={[{currentBalance: 124.50},{currentBalance: 500.23}]} />
+
+
+
     </section>
   )
 }
-
-export default Home
